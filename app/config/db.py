7715732d8cef9a680699334de69d38ad33376bd0
Ivanic <62161915+masterivanic@users.noreply.github.com>
2025-06-04 13:06:43 +0200

    feat: add compose and docker file
    * Update dockerfile and compose
    
    * chore: Update deploy stage

--- app/config/db.py
+++ app/config/db.py
@@ -1,7 +1,7 @@
 import os
- 
+
 from app.config.cfg import DEBUG
- 
+
 DB_MODELS = [
     "app.models.users",
     "app.models.pizza",
@@ -9,26 +9,26 @@ DB_MODELS = [
 ]
 POSTGRES_DB_URL = "postgres://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_db}"
 SQLITE_DB_URL = "sqlite://db.sqlite3"
- 
- 
+
+
 class PostgresSettings:
     postgres_user: str = os.getenv("POSTGRES_USER")
     postgres_password: str = os.getenv("POSTGRES_PASSWORD")
     postgres_db: str = os.getenv("POSTGRES_DB")
     postgres_port: str = os.getenv("POSTGRES_PORT")
     postgres_host: str = os.getenv("POSTGRES_HOST")
- 
- 
+
+
 class TortoiseSettings:
     def __init__(self, db_url: str, modules: dict, generate_schemas: bool):
         self.db_url = db_url
         self.modules = modules
         self.generate_schemas = generate_schemas
- 
+
     @classmethod
     def generate(cls):
         """Generate Tortoise-ORM settings (with sqlite if tests)"""
- 
+
         if DEBUG:
             db_url = SQLITE_DB_URL
         else:
@@ -37,5 +37,3 @@ class TortoiseSettings:
             del postgres
         modules = {"models": DB_MODELS}
         return TortoiseSettings(db_url=db_url, modules=modules, generate_schemas=True)
- 
- 
